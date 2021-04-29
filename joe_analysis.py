30ebda9e23d6f582012a9cbe19d2d005030b6782
Milo Knell <miloknell@gmail.com>
2021-04-29 16:13:54 -0700

    Create joe_analysis.py

---
+++ joe_analysis.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('joe_mega.csv')
+
+start = 5 #10 hour max #2.4
+N = 100 #40
+step = 0.001
+k = np.arange(start,N,step)
+
+
+def read_psd(filename):
+    with open(filename,'r') as f:
+        psd = f.read().strip().split('\n')
+    psd = np.array(psd)
+    return psd
+
+psd = read_psd('psd.txt')
+plt.plot(k**-1,psd)
+plt.show()
